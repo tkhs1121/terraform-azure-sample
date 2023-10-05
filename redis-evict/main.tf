90fb6e4392e44ff293f1c0b2293b37daaf984b80
tkhs1121 <tkhs1121@protonmail.com>
2023-10-05 17:30:07 +0900

    RedisのEvictさせる構成追加

---
+++ redis-evict/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "test" {
+  name     = "redis-rg"
+  location = "japaneast"
+}
+ 
+resource "azurerm_redis_cache" "test" {
+  name                = "test-redis-cache-${random_pet.test.id}-${random_integer.test.result}"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  capacity            = 1          # Redisキャッシュのサイズ
+  family              = "C"        # 価格タイプ, C(Basic/Standard), P(Premium)
+  sku_name            = "Standard" # SKU(Basic, Standard, Premium)
+  enable_non_ssl_port = true       # 非TLSポートの有効化
+
+  redis_configuration {
+    maxmemory_policy   = "allkeys-random"
+    maxmemory_reserved = 3855
+  }
+}
---
+++ redis-evict/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_pet" "test" {
+  length    = 1
+  separator = ""
+}
+
+resource "random_integer" "test" {
+  min = 1
+  max = 100
+}
